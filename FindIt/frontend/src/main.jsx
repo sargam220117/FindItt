6ebd4249e917b7c38ad45cb2c4f67cc79d602256
sargam220117 <sargam220117@gmail.com>
2025-12-22 00:31:33 +0530

    Backend Url update

--- FindIt/frontend/src/main.jsx
+++ FindIt/frontend/src/main.jsx
@@ -4,10 +4,11 @@ import App from './App.jsx';
 import './index.css';
 import axios from 'axios';
 
-axios.defaults.baseURL = import.meta.env.VITE_API_URL || 'http://localhost:5000';
+axios.defaults.baseURL = import.meta.env.VITE_API_URL || 'https://finditt-backend-bzyl.onrender.com';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
+
 );
