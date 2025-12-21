451c88b2797e079b5a971999e25f5d782762a9b3
sargam220117 <sargam220117@gmail.com>
2025-12-22 00:30:11 +0530

    Update API proxy URL in Vite configuration

--- FindIt/frontend/vite.config.js
+++ FindIt/frontend/vite.config.js
@@ -5,7 +5,8 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      '/api': 'http://localhost:5000'
+      '/api': 'https://finditt-backend-bzyl.onrender.com'
     }
   }
+
 })
