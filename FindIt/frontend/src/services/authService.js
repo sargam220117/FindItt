efd7aab712dc457a48f016776f7542b92c77aa3e
sargam220117 <sargam220117@gmail.com>
2025-12-22 00:36:52 +0530

    Update authService.js

--- FindIt/frontend/src/services/authService.js
+++ FindIt/frontend/src/services/authService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = `${import.meta.env.VITE_API_URL || 'http://localhost:5000'}/api/auth`;
+const API_URL = `${import.meta.env.VITE_API_URL || 'https://finditt-backend-bzyl.onrender.com'}/api/auth`;
 
 export const sendOtp = async (email) => {
     const response = await axios.post(`${API_URL}/send-otp`, { email });
